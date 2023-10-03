0d715f3b35eccaf5ec4719c6bbf383e1f8d007c3
Drew F Tomasovic <drewfrancis95@gmail.com>
2023-10-03 11:26:01 -0400

    update_7

--- src/AppleModel.java
+++ src/AppleModel.java
@@ -7,9 +7,31 @@ public class AppleModel {
     private int weight;
     private boolean ripe;
 
-    public int getWeight(){
-            return weight;
+
+    public void setColor(String color){
+        if(color.equalsIgnoreCase("red") || color.equalsIgnoreCase("green")){
+                this.color=color;
+        }
     }
+
+    public String getColor(){
+        return this.color;
+    }
+
+
+    public void setRipe(boolean ripe){
+        this.ripe = ripe;
+    }
+
+
+    public boolean isRipe(){
+        return this.ripe;
+    }
+
+    public int getWeight(){
+            return this.weight;
+    }
+    /* Set the give weight 0++ to 3- cannot be outside of range */
 
     public void setWeight(int weight){
         if(weight>= 0 && weight <= 3){
@@ -18,5 +40,6 @@ public class AppleModel {
     }
     /*This is an assignment variable,use THIS to define a parameter within a method, it references the weight class  */
     /* Goal is to make code highly readable */
+    /* String is an object, int and bool are primitive data types */
 }
 
